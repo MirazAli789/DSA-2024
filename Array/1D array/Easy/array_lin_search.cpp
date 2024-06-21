25e68bccc7ac69534527d326efd1ffea9817aa91
Miraz <2609miraj@gmail.com>
2024-06-21 16:15:36 +0530

    allchanges

---
+++ Array/1D array/Easy/array_lin_search.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool linear(int arr[],int key,int n){
+    for(int i = 0;i<n;i++){
+        if(arr[i]==key){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main()
+{
+    
+    cout<<endl;
+    return 0;
+}
