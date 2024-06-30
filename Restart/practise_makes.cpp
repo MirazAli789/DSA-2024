acaa89501d8843f801fe54ebe703a9fc41db9bf6
Miraz <2609miraj@gmail.com>
2024-06-30 23:24:08 +0530

    local

--- Restart/practise_makes.cpp
+++ Restart/practise_makes.cpp
@@ -1,28 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int gcd(int a,int b){
+int gcd(int a, int b)
+{
     int gcd = INT_MIN;
-    int mini = min(a,b);
-    for(int i= 1;i<=mini;i++){
-        if(a%i==0&&b%i==0){
-            gcd = max(gcd,i);
+    int mini = min(a, b);
+    for (int i = 1; i <= mini; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            gcd = max(gcd, i);
         }
     }
     return gcd;
 }
-int lcm(int a,int b){
+int lcm(int a, int b)
+{
     int lcm = INT_MAX;
-    int maxi = max(a,b);
-    for(int i=maxi;i<=(a*b);i++){
-        if(i%a==0&&i%b==0){
-            lcm = min(lcm,i);
+    int maxi = max(a, b);
+    for (int i = maxi; i <= (a * b); i++)
+    {
+        if (i % a == 0 && i % b == 0)
+        {
+            lcm = min(lcm, i);
         }
     }
     return lcm;
 }
 int main()
 {
-    cout<<gcd(3,15);
-    cout<<endl;
+    cout << gcd(3, 15) << endl;
+    cout << lcm(5, 20);
+    cout << endl; 
     return 0;
 }
