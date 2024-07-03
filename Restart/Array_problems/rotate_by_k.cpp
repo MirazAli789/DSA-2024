9003374a2383df1e8174e6f3fc929d6ef17bfcd2
Miraz <2609miraj@gmail.com>
2024-07-04 00:02:29 +0530

    welocoem

--- Restart/Array_problems/rotate_by_k.cpp
+++ Restart/Array_problems/rotate_by_k.cpp
@@ -8,10 +8,16 @@ vector<int> rotate_by_k(vector<int> arr, int k)
     {
         ans[(i + k) % n] = arr[i];
     }
+    return ans;
 }
 int main()
 {
-
+    vector<int> arr = {1, 2, 3, 4, 5};
+    vector<int> ans = rotate_by_k(arr, 3);
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
     cout << endl;
     return 0;
 }
