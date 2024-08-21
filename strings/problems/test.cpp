13154e5e8318037f75efa4a2e66c2a69180000a9
miraz <2609miraj@gmail.com>
2024-08-21 18:09:02 +0530

    uy

--- strings/problems/test.cpp
+++ strings/problems/test.cpp
@@ -1,38 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-void rev(string &s)
+bool is_pallindrome(string st, int s, int e)
 {
-    int start = 0, end = s.size() - 1;
-    while (start <= end)
+    if (s > e)
     {
-        swap(s[start], s[end]);
-        start++;
-        end--;
+        return true;
     }
+    else if (st[s] != st[e])
+    {
+        return false;
+    }
+    return is_pallindrome(st, s + 1, e - 1);
 }
 int main()
 {
-
-    // char arr[10];
-    // cout << "Enter the string\n";
-    // cin >> arr;
-    // arr[2] = '\0';
-    // cout << arr << endl;
-    // find the length of string
-    // char str1[10];
-    // cout << "Enter the string \n";
-    // cin >> str1;
-    // int len = 0;
-    // while (str1[len] != '\0')
-    // {
-    //     len++;
-    // }
-    // cout << "The length of the string is : " << len << endl;
-    // reverse a string
     string s;
-    cout << "Enter a string\n";
+    cout << "Enter a string \n";
     cin >> s;
-    rev(s);
-    cout << s << endl;
+    if (is_pallindrome(s, 0, s.size() - 1))
+    {
+        cout << "The string is a pallindrome\n";
+    }
+    else
+    {
+        cout << "The string is not a pallindrome\n";
+    }
     return 0;
 }
